aab118588498e20351841d3605fa04d638fd4848
I438161 <p.vanhooren@student.fontys.nl>
2022-06-17 15:47:20 +0200

    :green_heart: Another attempt at fixing Cypress in pipeline

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,12 @@
 describe('Authenticated tests', () => {
   it('Load homepage', () => {
     cy.visit(Cypress.env("url"))
-      cy.get('.btn-primary').click();
+      cy.get('.btn-primary').click({force: true});
   
       cy.origin("https://vybes.eu.auth0.com/", () => {
         cy.get("#username").type(Cypress.env("username"));
         cy.get("#password").type(Cypress.env("password"));
-        cy.get(".cf3cf4903 > .ce746f871").click();
+        cy.get(".cf3cf4903 > .ce746f871").click({force: true});
       });
 
       cy.get('h1').contains('Cypress');
@@ -14,38 +14,38 @@ describe('Authenticated tests', () => {
 
   it('Load profile page', () => {
     cy.visit(Cypress.env("url"))
-      cy.get('.btn-primary').click();
+      cy.get('.btn-primary').click({force: true});
   
       cy.origin("https://vybes.eu.auth0.com/", () => {
         cy.get("#username").type(Cypress.env("username"));
         cy.get("#password").type(Cypress.env("password"));
-        cy.get(".cf3cf4903 > .ce746f871").click();
+        cy.get(".cf3cf4903 > .ce746f871").click({force: true});
       });
       
-      cy.get('.dropdown-toggle').click();
-      cy.get('.dropdown-button:first-child').click();
+      cy.get('.dropdown-toggle').click({force: true});
+      cy.get('.dropdown-button:first-child').click({force: true});
 
       cy.get('h5').contains('Cypress');
   })
 
   it('Change display name', () => {
     cy.visit(Cypress.env("url"))
-      cy.get('.btn-primary').click();
+      cy.get('.btn-primary').click({force: true});
   
       cy.origin("https://vybes.eu.auth0.com/", () => {
         cy.get("#username").type(Cypress.env("username"));
         cy.get("#password").type(Cypress.env("password"));
-        cy.get(".cf3cf4903 > .ce746f871").click();
+        cy.get(".cf3cf4903 > .ce746f871").click({force: true});
       });
       
-      cy.get('.dropdown-toggle').click();
-      cy.get('.dropdown-button:first-child').click();
+      cy.get('.dropdown-toggle').click({force: true});
+      cy.get('.dropdown-button:first-child').click({force: true});
 
-      cy.get('.profile-button:first-child').click();
+      cy.get('.profile-button:first-child').click({force: true});
       cy.get('input.form-control:first-child').clear().type('Cypress e2e testing acc')
-      cy.get('.btn-primary').click()
+      cy.get('.btn-primary').click({force: true})
 
-      cy.get('.navbar-logo').click();
+      cy.get('.navbar-logo').click({force: true});
       cy.get('h1').contains('e2e')
   })
 })
